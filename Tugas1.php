69b2f101150b150c105619b953181d3568a8ffd0
ValenciaACH <22410100042@dinamika.ac.id>
2024-03-19 21:26:03 +0700

    224101000

---
+++ Tugas1.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+     <head>
+        <title>HTMLku</title>
+     </head>
+     <style>
+table, th, td {
+  border: 1px solid blue;
+  padding: 15px;
+td {
+    text-align: center
+}  
+    </style>
+     <body>
+     <a href="Tugas11.php" target="_self"><input type="button" name="+" value="+"></a>
+    <table border="1">
+	<tr>
+	    <th rowspan="2">kode</th>
+        <th rowspan="2">nama barang</th>
+	    <th rowspan="2">satuan</th>
+	     <th colspan="2">harga</th>	   
+	    <th rowspan="2">Action</th>	
+	</tr>
+
+	<tr>             
+	    
+	     <td style="width:20%">beli</td>
+	     <td style="width:20%">jual</td>
+
+	<tr>
+	     <td>M01</td>
+         <td>cpu</td>
+	     <td>pcs</td>
+	     <td>200</td>
+	     <td>350</td>
+	     <td>
+           
+		    <a href="Tugas12.php" target="_self"><input type="button" name="E" value="E"></a>
+           
+	     </td>		
+	</tr>
+
+    <tr>
+	     <td>M02</td>
+         <td>ram</td>
+	     <td>pcs</td>
+	     <td>300</td>
+	     <td>400</td>
+	     <td>
+         <a href="Tugas12.php" target="_self"><input type="button" name="E" value="E"></a>
+	     </td>		
+	</tr>
+
+    <tr>
+	     <td>M03</td>
+         <td>vga</td>
+	     <td>pcs</td>
+	     <td>400</td>
+	     <td>500</td>
+	     <td>
+         <a href="Tugas12.php" target="_self"><input type="button" name="E" value="E"></a>
+	     </td>		
+	</tr>
+    </table>
+     </body>
+</html>
---
+++ Tugas12.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<form actions="latihan2.php" method="POST">
+  <label for="kode">Kode:</label><br>
+  <input type="text" id="kode" name="kode"><br>
+  <label for="nama">Nama:</label><br>
+  <input type="text" id="nama" name="nama"><br>
+  <label for="satuan">Satuan:</label><br>
+  <input type="text" id="satuan" name="satuan"><br>
+  <label for="beli">Beli:</label><br>
+  <input type="text" id="beli" name="beli"><br> 
+  <label for="jual">Jual:</label><br>
+  <input type="text" id="jual" name="harga"><br> 
+  <a href="Tugas1.php" target="_self"><input type="update" value="Update" name="update"></a>
+  <a href="Tugas1.php" target="_self"><input type="delete" value="Delete" name="delete"></a>
+  <a href="Tugas1.php" target="_self"><input type="cancel" value="Cancel" name="cancel"></a>
+</form>
+</body>
+</html>
